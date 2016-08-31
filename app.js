67a8f213d8b577bc64409518cab72324d1d37157
Benjamin Sternthal <ben@devpatch.com>
2016-08-31 09:24:20 -0700

    Added callbacks so slack gets a chance to happen before the app barfs.

--- app.js
+++ app.js
@@ -26,24 +26,26 @@ function getData() {
         var wuJSON = {};
 
         if(err) {
-            bot.postMessageToGroup(channel, 'Error!' + err, params);
-            console.log(err);
-            process.exit(1);
+            bot.postMessageToGroup(channel, 'Error!' + err, params, function() {
+                console.log(err);
+                process.exit(1);
+            });
         }
 
         if(response.response.error) {
-            bot.postMessageToGroup(channel, 'Error!' + response.response.error.type, params);
-            console.log('Error: '+ response.response.error.type);
-            process.exit(1);
+            bot.postMessageToGroup(channel, 'Error!' + response.response.error.type, params, function() {
+                console.log('Error: '+ response.response.error.type);
+                process.exit(1);
+            });
+        } else {
+            wuJSON['current_temperature'] = response.current_observation.temp_c;
+            wuJSON['current_humidity'] = parseInt(response.current_observation.relative_humidity);
+            wuJSON['pressure_mb'] = response.current_observation.pressure_mb;
+
+            Influx.writeInflux(wuJSON);
+            setTimeout(getData, conf.get('update_frequency'));
         }
 
-
-        wuJSON['current_temperature'] = response.current_observation.temp_c;
-        wuJSON['current_humidity'] = parseInt(response.current_observation.relative_humidity);
-        wuJSON['pressure_mb'] = response.current_observation.pressure_mb;
-
-        Influx.writeInflux(wuJSON);
-        setTimeout(getData, conf.get('update_frequency'));
     });
 };
 
