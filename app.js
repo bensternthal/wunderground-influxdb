669e9816a064e932f366cca78612b027a8232fd8
Benjamin Sternthal <ben@devpatch.com>
2016-08-31 09:17:16 -0700

    Added better error checking, added update timeing to configuration.

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ var wunderground = new Wunderground(conf.get('wunderground_key'));
 
 bot.postMessageToGroup(channel, 'Weather Underground Has Started', params);
 
-
 function getData() {
 
     wunderground.conditions().request('97214', function(err, response){
@@ -32,12 +31,19 @@ function getData() {
             process.exit(1);
         }
 
+        if(response.response.error) {
+            bot.postMessageToGroup(channel, 'Error!' + response.response.error.type, params);
+            console.log('Error: '+ response.response.error.type);
+            process.exit(1);
+        }
+
+
         wuJSON['current_temperature'] = response.current_observation.temp_c;
         wuJSON['current_humidity'] = parseInt(response.current_observation.relative_humidity);
         wuJSON['pressure_mb'] = response.current_observation.pressure_mb;
 
         Influx.writeInflux(wuJSON);
-        setTimeout(getData, 300000);
+        setTimeout(getData, conf.get('update_frequency'));
     });
 };
 
